240867e793928cf2a1cc2532ddff0a02f68e3f50
The Lame Brain <thelamebrain@gmail.com>
2021-11-20 21:25:59 -0800

    Saturday Night Live Update!
    Ironed out the main play loop.
    You can respawn (I added a debug R button)
    Need to hook up to buttons that pop on UI to do that.
    Need to add a victory state when all waves cleared
    need to get teh Game state to manage transitioning between scenes
    and scene awareness

--- Assets/Scripts/I_am_the_player.cs
+++ Assets/Scripts/I_am_the_player.cs
@@ -19,7 +19,7 @@ public class I_am_the_player : MonoBehaviour
     private AudioSource sfxaudio;
     private Vector2 move;
     private GameObject Target_Object;
-    public bool isJumping, isKicking;
+    private bool isJumping, isKicking, isDead;
     [HideInInspector]
     public bool canControl = true;
 
@@ -44,7 +44,7 @@ public class I_am_the_player : MonoBehaviour
     private void Start()
     {
         GameManager.PLAYER = this;
-        GameManager.SetAudioMixer(audiomixer); //Passes the Audiomixer to the Gamemanager (debug)
+        GameManager.SetAudioMixer(audiomixer); //Passes the Audiomixer to the Gamemanager (debug)        
     }
 
     private void Update()
@@ -83,6 +83,11 @@ public class I_am_the_player : MonoBehaviour
             else
                 FindObjectOfType<UI_Controller>().Resume();
         }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            Respawn();
+        }
     }
 
     public void SetJump(bool b)
@@ -106,6 +111,12 @@ public class I_am_the_player : MonoBehaviour
         anim.SetBool("Kicking", b);
         isKicking = b;
         //Debug.Log("KICK CALLED: " + b);
+    }
+
+    public void SetDead(bool b)
+    {
+        anim.SetBool("Dead", b);
+        isDead = b;
     }
 
     private void FixedUpdate()
@@ -206,6 +217,7 @@ public class I_am_the_player : MonoBehaviour
         if (canControl)
         {
             canControl = false;
+            SetDead(true);
             move = Vector2.zero;
             anim.SetBool("Jumping", false);
             anim.SetBool("Kicking", false);
@@ -222,7 +234,8 @@ public class I_am_the_player : MonoBehaviour
         {
             anim.SetTrigger("Death");
             sfxaudio.PlayOneShot(FindSound("Player_Death"));
-            canControl = false;            
+            canControl = false;
+            SetDead(true);
         }
     }
 
@@ -231,6 +244,7 @@ public class I_am_the_player : MonoBehaviour
         if (canControl)
         {
             canControl = false;
+            SetDead(true);
             move = Vector2.zero;
             anim.SetBool("Jumping", false);
             anim.SetBool("Kicking", false);
@@ -240,6 +254,28 @@ public class I_am_the_player : MonoBehaviour
             sfxaudio.clip = FindSound("Player_Lavafall");
             sfxaudio.Play();
             canControl = false;
+        }
+    }
+
+    public void Respawn()
+    {
+        if(GameManager.Lives > 0)
+        {
+            GameManager.Lives--;
+            rb.velocity = Vector2.zero;
+            move = Vector2.zero;
+            SetDead(false);
+            SetKick(false);
+            SetJump(false);
+            anim.SetInteger("Move Y", 0);
+            anim.SetInteger("Move X", 0);
+            anim.ResetTrigger("Death");
+            anim.ResetTrigger("Falls");
+            anim.ResetTrigger("Melts");
+            anim.ResetTrigger("Dance");
+            anim.SetTrigger("Respawn");
+            transform.position = GameManager.PUZZLE.PlayerSpawn;
+            canControl = true;
         }
     }
 }
